4c0dd071a101f43fc28318cb42297d6a8d281f63
andrew <andrew@codechimp.org>
2015-03-06 15:49:30 +0000

    Wired up checkable buttons for conditional animation, similar to what you would use in a listview multi select

--- example/src/main/java/at/markushi/circlebutton/example/MainActivity.java
+++ example/src/main/java/at/markushi/circlebutton/example/MainActivity.java
@@ -12,54 +12,58 @@ import at.markushi.ui.CircleButton;
 
 public class MainActivity extends ActionBarActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    static int checkedCount = 0;
 
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
-		}
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-	public static class PlaceholderFragment extends Fragment implements View.OnClickListener {
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
+        }
+    }
 
-		public PlaceholderFragment() {
-		}
+    public static class PlaceholderFragment extends Fragment implements View.OnClickListener {
 
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-			final View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        public PlaceholderFragment() {
+        }
 
-			rootView.findViewById(R.id.button0).setOnClickListener(this);
-			rootView.findViewById(R.id.button1).setOnClickListener(this);
-			rootView.findViewById(R.id.button2).setOnClickListener(this);
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            final View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-            // Animated checkable button
-            rootView.findViewById(R.id.buttoncheck0).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    CircleButton cb = (CircleButton)v;
-                    cb.setChecked(!cb.getChecked());
-                }
-            });
+            rootView.findViewById(R.id.button0).setOnClickListener(this);
+            rootView.findViewById(R.id.button1).setOnClickListener(this);
+            rootView.findViewById(R.id.button2).setOnClickListener(this);
 
-            // Simple checkable button
-            rootView.findViewById(R.id.buttoncheck1).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    CircleButton cb = (CircleButton)v;
-                    cb.setChecked(!cb.getChecked());
-                }
-            });
+            // Two checkable buttons, wired up so only first/last on/off is animated
+            rootView.findViewById(R.id.buttoncheck0).setOnClickListener(new CheckableListener());
+            rootView.findViewById(R.id.buttoncheck1).setOnClickListener(new CheckableListener());
 
             return rootView;
-		}
+        }
 
-		@Override
-		public void onClick(View view) {
-			Toast.makeText(getActivity(), "Button clicked", Toast.LENGTH_SHORT).show();
-		}
-	}
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(getActivity(), "Button clicked", Toast.LENGTH_SHORT).show();
+        }
 
+        public static class CheckableListener implements View.OnClickListener {
+            @Override
+            public void onClick(View v) {
+                CircleButton cb = (CircleButton) v;
+
+                if (!cb.getChecked()) {
+                    checkedCount++;
+                    cb.setCheckAnimation((checkedCount == 1));
+                } else {
+                    cb.setCheckAnimation((checkedCount == 1));
+                    checkedCount--;
+                }
+
+                cb.setChecked(!cb.getChecked());
+            }
+        }
+    }
 }
